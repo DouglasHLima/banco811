38958bed54aaeda3d63ff8633bc123c2a61637b6
DouglasHLima <douglashdelima@gmail.com>
2022-04-12 00:00:35 -0300

    fix path

--- src/main/java/com/santander/banco811/security/SecurityConfig.java
+++ src/main/java/com/santander/banco811/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeHttpRequests()
-                .antMatchers(HttpMethod.POST, "/usuario").permitAll()
+                .antMatchers(HttpMethod.POST, "/user").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticateFilter(authenticationManager()))
